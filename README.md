04fbb4caf63d95295f32b306c3bda537fd503f02
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2023-05-22 00:32:38 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # example-expressjs
- 
+
+1. installation of expressjs
+2. install and configure testing framework - jest and supertest
+3. add 'hello world' example expressjs web server
---
+++ src/1_hello_world/index.test.js
@@ -0,0 +1,24 @@
+const supertest = require('supertest');
+const http = require('http');
+const { app } = require('./app.js');
+
+describe('Hello World! endpoint', () => {
+    let server;
+    let request;
+
+    beforeAll((done) => {
+        server = http.createServer(app);
+        server.listen(done);
+        request = supertest(server);
+    });
+
+    afterAll(done => {
+        server.close(done);
+    });
+
+    it('GET / should show a "Hello World!"', async () => {
+        const res = await request.get('/');
+        expect(res.status).toEqual(200);
+        expect(res.text).toEqual('Hello World!');
+    });
+});
