ea2ff8fb6e3faca7826103fe2cc72abb3257fbfe
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2023-05-22 01:25:28 +0200

    remove endpoint

--- src/4_static_routing/app.js
+++ src/4_static_routing/app.js
@@ -3,10 +3,6 @@ const path = require('path');
 
 const app = express()
 
-app.get('/', (_req, res) => {
-  res.send('Hello World!')
-})
-
 app.use('/static', express.static(path.join(__dirname, 'public')))
 
 module.exports = { app }
