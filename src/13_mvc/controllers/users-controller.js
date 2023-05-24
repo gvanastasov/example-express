ef36f647a020f9901bd16b9f2bcf249359c519bc
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2023-05-24 21:40:24 +0200

    add multi action controller

--- src/13_mvc/controllers/users-controller.js
+++ src/13_mvc/controllers/users-controller.js
@@ -4,6 +4,12 @@ const controller = {
     index: {
         method: 'get',
         action: function() {
+            return this.view();
+        }
+    },
+    show: {
+        method: 'get',
+        action: function() {
             return this.view({ 
                 viewBag: { 
                     users 
