0d04c0d00e4376bc3186694e006c4e32a67b8bc8
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2023-05-24 21:27:56 +0200

    add layout support

--- src/13_mvc/lib/route.js
+++ src/13_mvc/lib/route.js
@@ -2,16 +2,39 @@ const express = require('express'),
     fs          = require('fs'),
     path        = require('path')
 
+    /**
+     * @description this needs quite some love to be where it should be, but for now
+     * id does the job. Its used as prototype chain to controller module objects.
+     * We always want to render _layout, and then include the view from the control/action
+     * resolver and pass a viewBag to it.
+     * @param {*} options 
+     * @param {*} context 
+     */
 function baseController(options, context) {
     this.options = options;
     this.context = context;
     this.view = function(options) {
         let optionsInternal = { ...this.options, ...options };
         let viewName = optionsInternal.viewName || 'index';
-        this.context.responseContext.res.render(viewName, { viewBag: optionsInternal.viewBag });
+        this.context.responseContext.res.render(
+            'shared/_layout',
+            {
+                title: 'Example MVC',
+                body: "..\\" + viewName + ".html",
+                viewBag: optionsInternal.viewBag,
+            });
     }
 }
 
+/**
+ * @description this boots up the sub routing, by itterating over
+ * controllers folder. For each controller we identify actions (with 
+ * specific signature) which is then prototype chained to baseController
+ * in order to create route with a handler. 
+ * 
+ * @param {*} parent 
+ * @param {*} options 
+ */
 const generateRouteTable = function(parent, options = { default: { controller: 'home', action: 'index' }}) {
     const controllersRegex = /-controller.js$/;
     const controllersDirectory = path.join(__dirname, '..', 'controllers');
@@ -59,12 +82,13 @@ const generateRouteTable = function(parent, options = { default: { controller: '
             }, []);
 
             var app = express();
-            app.set('views', path.join(__dirname, '..', 'views', controllerName))
+            app.set('views', path.join(__dirname, '..', 'views'));
+            app.set('view engine', 'html');
 
             actions.forEach(action => {
                 let method = action.route.method || 'get';
                 let route = `/${controllerName}/${action.name}`;
-                let viewName = action.name;
+                let viewName = path.join(controllerName, action.name);
                 
                 let handler = (req, _res, _next) => {
                     function controller() {
